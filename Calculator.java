a005d266042acae50629f1beff2b9db571a4e047
Mitesh Sahu <72001481+MiteshSahu@users.noreply.github.com>
2022-02-22 14:42:21 +0530

    my first commit for jenkins

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Calculator
+{
+	public static void main(String arr[])
+	{
+		System.out.println("Hello");
+	}
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+	public int sub(int a,int b)
+	{
+		return a-b;
+	}
+	public int mul(int a,int b)
+	{
+		return a*b;
+	}
+}
